b7d0f5f054ea6f6480813972b7cbca365267ed0d
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2022-08-27 18:46:59 -0300

    draw rectangle on static canvas

---
+++ canvas_examples/01/canvas_01.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('canvas'); // gets canvas element
+var context = canvas.getContext('2d'); // gets canvas' context
+context.fillStyle = 'blue'; // sets fill color
+context.fillRect(100, 200, 150, 50); // fills a rectangle on x (100), y (200) having width (150) and height (50)
